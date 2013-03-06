be1b0d5c7038cbf7964e1104d063392634ae4080
Michael Richardson <mcr@credil.org>
2013-03-05 21:31:42 -0500

    first hack at adding constraints callbacks and refactoring session and scheduledslot objects

--- static/js/agenda_objects.js
+++ static/js/agenda_objects.js
@@ -83,32 +83,46 @@ function upperCaseWords(inp){
 
 var daysofweek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+// ScheduledSession is DJANGO name for this object, but needs to be renamed.
+// It represents a TimeSlot that can be assigned in this schedule.
+function ScheduledSlot() {}
 
 function slot_obj(scheduledsession_id, empty, timeslot_id, session_id, room, time, date, domid) {
-  this.scheduledsession_id = scheduledsession_id;
-  this.empty       = empty;
-  this.timeslot_id = timeslot_id;
-  this.session_id  = session_id;
-  this.date        = date;
-  this.time        = time;
-  this.room        = room;
-
-  var d = new Date(this.date);
-  var t = d.getUTCDay();
-  //console.log("short_string "+this.date+" gives "+t);
-    if(this.room == "Unassigned"){
-	this.short_string = "Unassigned";
+    ss = new ScheduleSlot();
+    ss.scheduledsession_id = scheduledsession_id;
+    ss.empty       = empty;
+    ss.timeslot_id = timeslot_id;
+    ss.session_id  = session_id;
+    ss.date        = date;
+    ss.time        = time;
+    ss.room        = room;
+    
+    var d = new Date(ss.date);
+    var t = d.getUTCDay();
+    //console.log("short_string "+ss.date+" gives "+t);
+    if(ss.room == "Unassigned"){
+	ss.short_string = "Unassigned";
     }
     else{
-	this.short_string = daysofweek[t] + ", "+ this.time + ", " + upperCaseWords(this.room);
+	ss.short_string = daysofweek[t] + ", "+ ss.time + ", " + upperCaseWords(ss.room);
     }
-  if(domid) {
-    this.domid = domid;
-  } else {
-    this.domid = json_to_id(this);
-    console.log("gen "+timeslot_id+" is domid: "+this.domid);
-  }
+    if(domid) {
+	ss.domid = domid;
+    } else {
+	ss.domid = json_to_id(this);
+	console.log("gen "+timeslot_id+" is domid: "+ss.domid);
+    }
+    return ss;
 }
+
+ScheduledSlot.prototype.session = function() {
+    if(this.session_id != undefined) {
+	return meeting_objs[this.session_id];
+    } else {
+	return undefined;
+    }
+};
+
 
 // SESSION OBJECTS
 // really session_obj.
@@ -130,36 +144,110 @@ function event_obj(title, description, session_id, owner, group_id, area) {
     session.owner = owner;
     session.area  = area;
     session.group_id = group_id;
+    session.loaded = false;
+    session.href = meeting_base_url+'/session/'+scheduledtimeslot.session_id+".json";
     return session;
 }
 
-Session.prototype.load_session_obj = function() {
-    if(!this.href) {
-        newobj = retrieve_session_by_id(this.session_id);
-        if(newobj) {
-            $.extend(this, newobj);
-        }
-    }
-}
 
-Session.prototype.group = function() {
-    this.load_session_obj();
-    if(!this.group_obj) {
-        console.log("looking for "+this.group_href);
-        this.group_obj = find_group_by_href(this.group_href);
+Session.prototype.load_session_obj = function(andthen, arg) {
+    if(!this.loaded) {
+	var oXMLHttpRequest = XMLHttpGetRequest(this.href, true);
+	oXMLHttpRequest.onreadystatechange = function() {
+	    if (this.readyState == XMLHttpRequest.DONE) {
+		try{
+		    last_json_txt = this.responseText;
+		    session_obj   = JSON.parse(this.responseText);
+		    //console.log("parsed: "+constraint_list);
+		    last_json_reply = constraint_list;
+		    $.extend(this, session_obj);
+		    this.loaded = true;
+		    if(andthen != undefined) {
+			andthen(this, true, arg);
+		    }
+		}
+		catch(exception){
+		    console.log("exception: "+exception);
+		    andthen(this, false, arg);
+		}
+	    }
+	};
+	oXMLHttpRequest.send();
+    } else {
+	if(andthen != undefined) {
+	    andthen(this, true, arg);
+	}
     }
-    // now have this.group_href.
+};
+
+Session.prototype.generate_info_table = function(ss) {
+    $("#info_grp").html(this.group_acronym);
+    $("#info_name").html(name_select_html);
+    $("#info_area").html("<span class='"+this.area.toUpperCase()+"-scheme'>"+this.area+"</span>");
+    $("#info_duration").html(this.requested_duration);
+
+    $("#info_location").html(generate_select_box()+"<button id='info_location_set'>set</button>");
+
+    // XXX we use *GLOBAL* current_timeslot rather than ss.timeslot_id!!!
+    $("#info_name_select").val(ss.timeslot_id);
+    $("#info_name_select").val($("#info_name_select_option_"+current_timeslot).val());
+
+    var temp_1 = $("#info_name_select_option_"+current_timeslot).val();
+    $("#info_name_select_option_"+ss.scheduledsession_id).css('background-color',highlight);
+
+    $("#info_location_select").val(ss.timeslot_id);
+    //console.log("git "+"#info_location_select_option_"+this.timeslot_id);
+    $("#info_location_select").val($("#info_location_select_option_"+ss.timeslot_id).val());
+
+    $("#"+ss.timeslot_id).css('background-color',highlight);
+
+    listeners();
+    
+    $("#info_responsible").html(this.responsible_ad);
+    $("#info_requestedby").html(this.requested_by +" ("+this.requested_time+")");
+};
+
+
+Session.prototype.group = function(andthen) {
+    if(!this.group_obj) {
+	this.group_obj = new Group();
+        console.log("looking for "+this.group_href);
+	this.group_obj.load_group_obj(this.group_href);
+    }
     return this.group_obj;
 }
 
 // GROUP OBJECTS
-function make_group_obj(obj) {
+function Group() {}
+Group.prototype.load_group_obj = function(href) {
+    this.href = href;
+
+    console.log("group "+href);
+
+    var oXMLHttpRequest = XMLHttpGetRequest(href, true);
+    if(oXMLHttpRequest.readyState == XMLHttpRequest.DONE) {
+        try{
+            console.log("parsing: "+oXMLHttpRequest.responseText);
+            last_json_txt = oXMLHttpRequest.responseText;
+            group_obj     = JSON.parse(oXMLHttpRequest.responseText);
+	    if(group_obj) {
+		//console.log("parsed: "+constraint_list);
+		last_json_reply = group_obj;
+		$.extend(this, group_obj);
+	    }
+        }
+        catch(exception){
+            console.log("retrieve group_by_href exception: "+exception);
+        }
+    }
+    oXMLHttpRequest.send();
 }
 
 function find_group_by_href(href) {
     console.log("group href", href, group_objs[href]);
     if(!group_objs[href]) {
-        retrieve_group_by_href(href);
+	group_objs[href]=new Group();
+	group_objs[href].load_group_obj(href);
     }
     return group_objs[href];
 }
